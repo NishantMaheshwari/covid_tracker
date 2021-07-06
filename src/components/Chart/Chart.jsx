3ffc2e3babb0f0957ecced6e74dc88701dec7576
NishantMaheshwari <86288838+NishantMaheshwari@users.noreply.github.com>
2021-07-06 08:21:27 +0530

    Add files via upload

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,67 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
+import {fetchDailyData} from "../../api";
+import { Line,Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
-const Chart = () => {
+const Chart = ({data:{confirmed,recovered,deaths},country}) => {
+   const [dailyData, setdailyData] = useState([]);
+
+  useEffect(()=>{
+   const fetchAPI=async()=>{
+     setdailyData(await fetchDailyData());
+   }
+
+   fetchAPI();
+
+  },[]);
+
+  const lineChart=(
+    dailyData.length!==0 ? 
+    (<Line
+      data={{
+        labels: dailyData.map(({date})=>date),
+        datasets:[{
+            data: dailyData.map(({confirmed})=>confirmed),
+            label: 'Infected',
+            bordercolor: 'black',
+            backgroundColor: 'blue',
+            fill : true,
+        },{
+            data: dailyData.map(({deaths})=>deaths),
+            label: 'Deaths',
+            bordercolor: 'red',
+            backgroundColor: 'rgba(255,0,0,0.5)',
+            fill : true,
+        }],
+      }}
+    />):null
+  );
+
+  const barChart=(
+    confirmed?
+    (
+      <Bar 
+        data={{
+         labels:['Infected','Recovered','Deaths'],
+         datasets:[{
+           label:`Current state in ${country}`,
+           backgroundColor:[
+             'rgb(0, 0, 255, 0.5)','rgb(0, 255, 0, 0.5)','rgb(255, 0, 0, 0.5)'
+           ],
+           data:[confirmed.value,recovered.value,deaths.value]
+         }]
+        }}
+         options={{
+           legend: {dispay:false},
+          // title: {display:true, text:`Current state in ${country}`},
+         }}
+      />
+    ):null
+  )
+
   return (
-    <div>
-      <h1>Chart</h1>
+    <div classname={styles.container}>
+      {country?barChart:lineChart}
     </div>
   );
 };
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
+import { NativeSelect,FormControl } from "@material-ui/core";
+import styles from "./CountryPicker.module.css";
+import {fetchCountries} from "../../api";
 
-const CountryPicker = () => {
+const CountryPicker = ({handleCountryChange}) => {
+
+  const [fetchedCountries, setfetchedCountries] = useState([]);
+
+ useEffect(()=>{
+   const fetchAPI=async()=>{
+    setfetchedCountries(await fetchCountries());
+   }
+   fetchAPI();
+ },[setfetchedCountries]);
+
   return (
     <div>
-      <h1>CountryPicker</h1>
+      <FormControl className={styles.formControl} style={{minWidth: 120}}>
+        <NativeSelect  className={styles.container} defaultValue="" onChange={(e)=>handleCountryChange(e.target.value)}>
+          <option value="">Global</option>
+          {fetchedCountries.map((country,i)=><option key={i} value={country}>{country}</option>)}
+        </NativeSelect>
+      </FormControl>
+      <FormControl className={styles.formControl} style={{minWidth: 120}}>
+        <NativeSelect className={styles.container} defaultValue="" onChange={(e)=>handleCountryChange(e.target.value)}>
+          <option value="">State</option>
+          <option value="">Saint vincent and the grenadines</option>
+        </NativeSelect>
+      </FormControl>
+      <FormControl className={styles.formControl} style={{minWidth: 120}}>
+        <NativeSelect className={styles.container}  defaultValue="" onChange={(e)=>handleCountryChange(e.target.value)}>
+          <option value="">District</option>
+          <option value="">Saint vincent and the grenadines</option>
+        </NativeSelect>
+      </FormControl>
     </div>
   );
 };
